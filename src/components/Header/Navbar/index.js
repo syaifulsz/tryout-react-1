da0522563f5ad449c099950110ea279432a3645c
Syaiful Shah Zinan <syaifulshahzinan@gmail.com>
2021-04-26 11:52:26 +0800

    Add class state for Navbar and config for pages

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -1,9 +1,40 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ConfigPages from '../../../configs/pages';
+
+const NavItem = ( { page, onClick, activePageSlug } ) => {
+
+    const linkClassNames = [ 'nav-link' ];
+    if ( activePageSlug === page.slug ) {
+        linkClassNames.push( 'active' );
+    }
+    const linkClassNamesRender = linkClassNames.join( ' ' );
+    return(
+        <li className="nav-item">
+            <Link className={linkClassNamesRender} onClick={ onClick } to={page.url}>{page.title}</Link>
+        </li>
+    )
+}
 
 export default class Navbar extends Component
 {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            activePageSlug: 'home'
+        };
+    }
+
     render() {
+        const navs = [];
+        for ( const key in ConfigPages ) {
+            const page = ConfigPages[ key ];
+            navs.push(<NavItem key={key} page={page} activePageSlug={ this.state.activePageSlug } onClick={ () => {
+                this.setState( {
+                    activePageSlug: page.slug
+                } )
+            } } />);
+        }
 
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
@@ -13,12 +44,7 @@ export default class Navbar extends Component
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
+                        {navs}
                     </ul>
                 </div>
             </nav>
---
+++ src/configs/pages/index.js
@@ -0,0 +1,18 @@
+const config = {
+    home: {
+        title: 'Home',
+        slug: 'home',
+        url: '/',
+        description: 'This is the home page description',
+        body: 'This is the content of home page'
+    },
+    about: {
+        title: 'About',
+        about: 'about',
+        url: '/about',
+        description: 'This is the about page description',
+        body: 'This is the content of about page'
+    }
+}
+
+export default config;
